6226c88e8841f53a29a1c242cc3268e7827842bf
AndoHopar <64177983+AndoHopar@users.noreply.github.com>
2023-04-22 02:26:11 -0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("create-account-button");
-const textArea = document.getElementById("generated-mnemonic");
-
-button.addEventListener("click", function() {
-  const mnemonic = bip39.generateMnemonic();
-  textArea.value = mnemonic;
-});
-
-
